49500bc44201470906c20899a469763387db32c0
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-12-14 18:51:39 +0700

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = new int[]{10, 3, 586, 4};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+
+    public static int[] bubbleSort(int[] arr) {
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    isSorted = false;
+                    buf = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = buf;
+                }
+            }
+
+        }
+
+        return arr;
     }
 }
+
+
